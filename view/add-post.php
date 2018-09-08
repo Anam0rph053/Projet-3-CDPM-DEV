74b9ce05c3790424bacc60c27ed230a002a28e03
littleBlackMan <littleblackman@users.noreply.github.com>
2018-09-08 12:35:50 +0200

    Update add-post.php
    use the PHP syntax alternative

--- view/add-post.php
+++ view/add-post.php
@@ -1,30 +1,20 @@
 
-<?php
-if (isset($_SESSION['alertes']['submit_success']) && !empty($_SESSION['alertes']['submit_success']))
-{
-    ?>
+<?php if (isset($_SESSION['alertes']['submit_success']) && !empty($_SESSION['alertes']['submit_success'])): ?>
     <div class="alert alert-success" role="alert">
         <p style="color:green;"><?= $_SESSION['alertes']['submit_success'] ?></p></div>
-    <?php
-    $_SESSION['alertes'] = [];
-}
-elseif (isset($_SESSION['alertes']['submit_error']) && !empty($_SESSION['alertes']['submit_error']))
-{
-    ?>
+    <?php $_SESSION['alertes'] = [];?>
+<?php elseif (isset($_SESSION['alertes']['submit_error']) && !empty($_SESSION['alertes']['submit_error'])):?>
     <div class="alert alert-danger" role="alert">
         <p style="color:darkred;"><?= $_SESSION['alertes']['submit_error'] ?></p></div>
-    <?php
-    $_SESSION['alertes'] = [];
-}
-?>
+    <?php $_SESSION['alertes'] = [];?>
+<?php endif;?>
 
-<h3 class="card-header text-center font-weight-bold text-uppercase py-4">Ajouter un Chapitre</h3><br/>
-
-
+<h3 class="card-header text-center font-weight-bold text-uppercase py-4">
+    Ajouter un Chapitre
+</h3>
+<br/>
 
 <form action="<?=HOST;?>add-post" method="post" enctype="multipart/form-data">
-
-
 
     <div class="md-form">
         <i class= "fas fa-user-secret prefix grey-text"></i>
